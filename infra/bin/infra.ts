1d0fc61b7d785c5a97e167768a48afbf4ee7e664
jayli <jayli@example.com>
2026-02-20 22:22:23 +1100

    feat: Implement backend CI/CD pipeline with AWS CodePipeline, S3 artifact storage, and GitHub Actions for deployment. v2

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { InfraStack } from '../lib';
+import { BackendPipelineStack } from '../lib/ci/backend-pipeline-stack';
 
 const app = new cdk.App();
 
@@ -8,5 +9,9 @@ const account = process.env.CDK_DEFAULT_ACCOUNT!;
 const region = process.env.CDK_DEFAULT_REGION!;
 
 new InfraStack(app, 'InfraStack', {
+  env: { account: account, region: region },
+});
+
+new BackendPipelineStack(app, 'DWHIBackendPipelineStack', {
   env: { account: account, region: region },
 });
---
+++ infra/config/backend/config.ci.ts
@@ -0,0 +1,5 @@
+export const ciConfig = {
+    backendGithubOidcRepo: 'Jayli58/do-we-have-it-backend',
+    infraSourceObjectKey: 'infra-source.zip',
+    backendSourceObjectKey: 'backend-source.zip',
+};
